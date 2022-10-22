f416e739750b4bfc85f203428ae9f60bbe614e31
ngchisern <ncsern2000@gmail.com>
2022-10-22 15:46:08 +0800

    Set up the texture object for storing the reflection

--- lab 3/e0550524_lab3/main.cpp
+++ lab 3/e0550524_lab3/main.cpp
@@ -570,10 +570,13 @@ void SetUpTextureMaps( void )
     //****************************
     // WRITE YOUR CODE HERE.
     //****************************
-
     // copy pasting
-
-
+    glGenTextures( 1, &reflectionTexObj );
+    glBindTexture( GL_TEXTURE_2D, reflectionTexObj );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR );
 }
 
 
